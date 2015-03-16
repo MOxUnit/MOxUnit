1efd90eba3268ffcc26cd5ea895e49cef9ef837a
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-03-16 16:48:02 +0100

    NF: when running the suite, use default MoxUnitTestResult object when it is not provided as the second argument

--- MOxUnit/@MOxUnitTestSuite/run.m
+++ MOxUnit/@MOxUnitTestSuite/run.m
@@ -1,4 +1,8 @@
 function result=run(obj,result)
+    if nargin<2
+        result=MOxUnitTestResult();
+    end
+
     for j=1:numel(obj.tests)
         test_case=obj.tests{j};
         result=run(test_case, result);
