4405cd60ff98a191831d89eea60691badd31c975
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-03-16 16:47:20 +0100

    ENH: let test fail when there is a syntax error, rather than just skip the file silently

--- MOxUnit/@MOxUnitTestSuite/addFromFile.m
+++ MOxUnit/@MOxUnitTestSuite/addFromFile.m
@@ -9,19 +9,27 @@ function obj=addFromFile(obj,fn)
 
     try
         func=str2func(name);
-
         test_case=func();
-
         if isa(test_case,'MOxUnitTestNode')
             obj=addTest(obj,test_case);
         end
     catch
+        caught_error=lasterror();
+
+        if is_parse_error(caught_error)
+            % add a test case that shows the parsing error
+            subfunction_name='';
+            func=@()rethrow(caught_error);
+            test_case=MOxUnitFunctionHandleTestCase(subfunction_name,...
+                                                    fn, func);
+            obj=addTest(obj,test_case);
+        else
+            % ignore silently, assuming that the file was not a test case
+        end
     end
 
 
-
-
-
-
-
+function tf=is_parse_error(caught_error)
+    tf=isempty(caught_error.identifier) && ...
+            ~isempty(regexp(caught_error.message,'^parse error','once'));
 
