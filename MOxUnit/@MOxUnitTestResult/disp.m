7aadf76f4af5c630fd577bcbe95610a2740cf4e9
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-08-04 13:59:47 +0200

    RF+ENH+DOC: do not use magic numbers, document options, and ensure that all output is written to the correct stream

--- MOxUnit/@MOxUnitTestResult/disp.m
+++ MOxUnit/@MOxUnitTestResult/disp.m
@@ -1,53 +1,93 @@
 function disp(obj)
-    fprintf(obj.stream,'\n\n');
+% Display test results
+%
+% disp(obj)
+%
+%   obj             MOxUnitTestResult instance.
+%
+% Notes:
+%   - this function writes results to the file descriptor provided when
+%     obj was instantiated, and with the provided verbosity level
+%
+% NNO 2015
 
-    horline=sprintf('%s\n\n',repmat('-',1,50));
 
-    keys={'errors','failures','skips','successes'};
+    horizontal_line=sprintf('%s\n\n',repmat('-',1,50));
 
-    % define minimum verbosity level for each key to show result
-    required_verbosity=[1 1 2 Inf];
 
-    counts=cellfun(@(x)numel(obj.(x)),keys);
+    if obj.verbosity>0
+        % write white-space
+        fprintf(obj.stream,'\n\n');
+    end
 
-    % only has non successes if either error or failure
-    has_non_successes=sum(counts(1:3));
+    % define minimum verbosity level for each test outcome to show result
+    required_verbosity=struct();
+    required_verbosity.errors=1;
+    required_verbosity.failures=1;
+    required_verbosity.skips=2;
+    required_verbosity.successes=Inf; % never show summary of success
 
-    for j=1:numel(keys)
-        if obj.verbosity>=required_verbosity(j) && counts(j)>0
-            fprintf(obj.stream, horline);
-            show_non_successes(obj,keys{j});
+    % define keys for non-success, and for failure
+    % (skips are not a success but not a failure either)
+    non_success_keys={'errors','failures','skips'};
+    failure_keys={'errors','failures'};
+
+    all_keys=fieldnames(required_verbosity);
+    get_counts=@(key_cell) cellfun(@(x)numel(obj.(x)),key_cell);
+
+    % show all errors, failures, and skips (if any).
+    % skips are only shown with verbose output; errors and failures are
+    % always shown.
+    all_counts=get_counts(all_keys);
+    for j=1:numel(all_keys)
+        key=all_keys{j};
+        if obj.verbosity>=required_verbosity.(key) && ...
+                    all_counts(j)>0
+            fprintf(obj.stream, horizontal_line);
+            show_non_successes(obj,key);
         end
     end
 
+    % show summary of result
     if obj.verbosity>0
-        fprintf(obj.stream,horline);
+        fprintf(obj.stream,horizontal_line);
 
-        is_ok=sum(counts(1:2))==0;
+        is_ok=sum(get_counts(failure_keys))==0;
         if is_ok
+            % only successes and skips
             prefix='OK';
         else
+            % at least one error or failure
             prefix='FAILED';
         end
 
+        non_success_counts=get_counts(non_success_keys);
+        has_non_successes=sum(non_success_counts)>0;
+
         if has_non_successes
+            % build string showing number of errors, failures, and skips,
+            % for example '(failures=1, skips=3)'
+
+            % start with open parenthesis
             postfix=' (';
-            non_success_indices=find(counts(1:3)>0);
+            non_success_indices=find(non_success_counts>0);
             for k=1:numel(non_success_indices)
                 non_success_index=non_success_indices(k);
                 if k>1
+                    % add comma
                     postfix=sprintf('%s, ',postfix);
                 end
 
-
+                % add string 'key=count'
                 postfix=sprintf('%s%s=%d',postfix,...
-                                        keys{non_success_index},...
-                                        counts(non_success_index));
+                                    non_success_keys{non_success_index},...
+                                    non_success_counts(non_success_index));
             end
+            % close parenthesis
             postfix=sprintf('%s)', postfix);
         else
             postfix='';
         end
 
-        fprintf('%s%s\n', prefix, postfix);
+        fprintf(obj.stream,'%s%s\n', prefix, postfix);
     end
