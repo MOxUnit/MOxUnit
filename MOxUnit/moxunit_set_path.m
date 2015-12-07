90412927357d580c2549a217907682e77bf88183
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-12-07 14:49:28 +0100

    SML: indent code

--- MOxUnit/moxunit_set_path.m
+++ MOxUnit/moxunit_set_path.m
@@ -6,10 +6,11 @@ function moxunit_set_path()
 % This adds the current directory and the 'util' test directory to the
 % search path
 
-root_dir=fileparts(which(mfilename()));
+    root_dir=fileparts(which(mfilename()));
 
-sub_dirs={'','util'};
+    sub_dirs={'','util'};
 
-full_dirs=cellfun(@(sd)fullfile(root_dir,sd),sub_dirs,...
-                    'UniformOutput',false);
-addpath(strjoin(full_dirs,pathsep()));
+    full_dirs=cellfun(@(sd)fullfile(root_dir,sd),sub_dirs,...
+                        'UniformOutput',false);
+    addpath(strjoin(full_dirs,pathsep()));
+
