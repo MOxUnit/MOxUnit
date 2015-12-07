6b499bbef3b9a17858c8679c585c3e86acd4b430
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-12-07 14:05:15 +0100

    ENH: add moxunit_set_path function that sets the search path

---
+++ MOxUnit/moxunit_set_path.m
@@ -0,0 +1,15 @@
+function moxunit_set_path()
+% sets the search path for MOxUnit
+%
+% moxunit_set_path()
+%
+% This adds the current directory and the 'util' test directory to the
+% search path
+
+root_dir=fileparts(which(mfilename()));
+
+sub_dirs={'','util'};
+
+full_dirs=cellfun(@(sd)fullfile(root_dir,sd),sub_dirs,...
+                    'UniformOutput',false);
+addpath(strjoin(full_dirs,pathsep()));
