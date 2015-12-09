ac7fe9343133e00eab1be87f87c68e1fe6e84056
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-12-09 16:20:31 +0100

    TST: add test for function handle test case

---
+++ tests/test_function_handle_test_case.m
@@ -0,0 +1,38 @@
+function test_suite=test_function_handle_test_case
+    initTestSuite;
+
+function test_function_handle_test_case_basics
+    rand_str=@()char(20*rand(1,10)+65);
+    
+    outcome_class2func=struct();
+    outcome_class2func.MOxUnitPassedTestOutcome=@do_nothing;
+    outcome_class2func.MOxUnitSkippedTestOutcome=@()...
+                            moxunit_throw_test_skipped_exception('');
+    outcome_class2func.MOxUnitFailedTestOutcome=@()error('here');
+    
+    keys=fieldnames(outcome_class2func);
+    for k=1:numel(keys)
+        outcome_class=keys{k};
+        func=outcome_class2func.(outcome_class);
+        
+        name=rand_str();
+        location=rand_str();
+        
+        f=MOxUnitFunctionHandleTestCase(name, location, func);
+        
+        rep=MOxUnitTestReport(0,1);
+        rep=run(f,rep);
+        
+        assertEqual(countTestOutcomes(rep),1);
+        outcome=getTestOutcome(rep,1);
+        
+        assertEqual(class(outcome),outcome_class);
+    end
+    
+        
+        
+    
+    
+function do_nothing()
+    % do nothing
+    
