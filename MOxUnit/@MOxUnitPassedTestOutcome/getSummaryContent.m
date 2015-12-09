bcd7ba5cf7f24aa70bb28ae31af0c6b32e3ea390
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2015-12-09 11:28:24 +0100

    BF: return empty content for passed test

--- MOxUnit/@MOxUnitPassedTestOutcome/getSummaryContent.m
+++ MOxUnit/@MOxUnitPassedTestOutcome/getSummaryContent.m
@@ -1,15 +1,14 @@
-function error_struct=getSummaryContent(obj)
+function content=getSummaryContent(obj)
 % return the error struct that made the test fail
 %
-% c=getSummaryContent(obj)
+% content=getSummaryContent(obj)
 %
 % Input:
 %   obj                     MoxUnitPassedTestOutcome object
 %
 % Output:
-%   error_struct            Struct containing the lasterror() contents that
-%                           made the test fail
+%   content                 The empty array []
 %
 % See also: lasterror
 
-    error_struct=obj.error;
+    content=[];
