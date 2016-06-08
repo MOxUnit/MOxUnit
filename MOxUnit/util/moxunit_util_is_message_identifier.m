5309cb57a6690bc1d1aa4a519d1c346a5a669ee5
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2016-06-08 16:43:18 +0200

    NF: help function to indicate whether a string is a message identifier

---
+++ MOxUnit/util/moxunit_util_is_message_identifier.m
@@ -0,0 +1,30 @@
+function tf=moxunit_util_is_message_identifier(id)
+% returns whether the input string is a message identifier
+%
+% tf=moxunit_util_is_message_identifier(string)
+%
+% Input:
+%   id              input string
+%
+% Output
+%   tf              true if the input string is a message identifier. A
+%                   message identifier is a string with the following
+%                   properties:
+%                   - contains only alphanumeric characters, and/or the
+%                     underscore character ('_') and colon (':')
+%                   - it contains at least one colon
+%                   - the first character is an alphabetic character
+%                   - every colon is immediately followed by an alphabetic
+%                     character
+
+    if ~ischar(id)
+        error('illegal input: first argument must be char');
+    end
+
+    alpha_pat='[a-zA-Z]';
+    word_pat='\w';
+
+    id_pat=sprintf('(%s(%s*))',alpha_pat,word_pat);
+    pat=sprintf('^%s(:%s)+$',id_pat,id_pat);
+
+    tf=~isempty(regexp(id, pat, 'once'));
