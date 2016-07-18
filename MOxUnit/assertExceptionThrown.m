87637667570f3a81e0fc7db99461a5c27a0872f0
Joel LeBlanc <jwleblan@gmail.com>
2016-07-18 16:56:30 -0400

    Reverting assertExceptionThrown.m to "mine"
    Somehow ended up with the wrong version of this file...

--- MOxUnit/assertExceptionThrown.m
+++ MOxUnit/assertExceptionThrown.m
@@ -1,71 +1,120 @@
-function assertExceptionThrown(func, expected_id, message)
-% assert that an exception is thrown
+% assertExceptionThrown - Assert that an exception is thrown
+%{ 
+%-------------------------------------------------------------------------------
+% SYNTAX:
+%   assertExceptionThrown(func, <expectedID>, <message>)
 %
-% assertExceptionThrown(func,[expected_id],[msg])
+% PURPOSE:
+%   This function allows one to test for exceptions being thrown, and
+%   optionally, pass a custome message in response to a failure.
 %
-% Inputs:
-%   func            function handle that should take no inputs and should
-%                   raise an exception
-%   expected_id     the idenitifier of the exception that func should
-%                   raise
-%   msg             optional custom message
+%   e.g.
+%   % Assert that sin will throw when its argument is a struct
+%   >> assertExceptionThrown( @()sin( struct([]) ) )
 %
-% Throws:
-%   'moxunit:exceptionNotRaised'    func() does not raise an exception
-%   'moxunit:wrongExceptionRaised'  func() does raise an exception but with
-%                                       an identifier different from
-%                                       expected_id
+%   % Assert that sin throws AND that the ID is 'MATLAB:UndefinedFunction'
+%   >> assertExceptionThrown( @()sin( struct([]) ), 'MATLAB:UndefinedFunction')
+%  
+% INPUT:
+%   func        - Function handle that is expected to throw, with the prototype
+%                   [varargout{:}] = func()
+%   expectedID  - Idenitifier of the expected exception.  If expectedID is
+%                 empty, then any exception (identifier) is allowed.  When 
+%                 exactly two inputs are passed an ambiguity arises between
+%                 expectedID and message because these inputs share a common
+%                 type.  By default, we resolve this ambiguity using
+%                 moxunit_util_is_message_identifier().  If any exception is
+%                 permitted, we recommend explicitly defining expectedID to be
+%                 empty.
+%                   Default: ''
+%   message     - Custom message to be included when func fails to throw
+%   
+% THROWS:
+%   'moxunit:exceptionNotRaised'    - func() does not raise an exception but was
+%                                     expected to do so.
+%   'moxunit:wrongExceptionRaised'  - func() does raise an exception but with
+%                                     an identifier different from expected_id
+%
+% ASSUMPTIONS: 
+%   All input variables are of the correct type, valid (if applicable), and 
+%   given in the correct order. 
+%
+% See also moxunit_util_is_message_identifier
+%-------------------------------------------------------------------------------
+%}
+function assertExceptionThrown(func, expectedID, message)
 
-    % Default Values
-    exception_was_raised = false;
-    expected_id_passed = nargin>1;
-    
-    
-    % Check func for an exception and capture it
-    if moxunit_util_platform_is_octave()
-        try
-            func();
-        catch
-            exception_was_raised=true;
-            [unused,found_id]=lasterr();
-        end
-    else
-        try
-            func();
-        catch mexception;
-            exception_was_raised=true;
-            found_id=mexception.identifier;
-        end
-    end
-
-    % Check for that exception meeting an id requirement
-    if ~exception_was_raised
-        error_id = 'moxunit:exceptionNotRaised';
-        if expected_id_passed
-            whatswrong = sprintf('expected exception ''%s'' was not raised',...
-                expected_id);
+% Parse Inputs
+switch nargin
+    case 1
+        expectedID = '';
+        message = '';
+        
+    case 2
+        if moxunit_util_is_message_identifier(expectedID)
+            message = '';
         else
-            whatswrong = 'exception was not raised';
+            message = expectedID;
+            expectedID = '';
         end
+        
+    case 3
+        % Do nothing
+        
+    otherwise
+        error('assertExceptionThrown:unexpectedInput',...
+            'Unexpected number of inputs');
+end
+
+
+% Check func for an exception and capture it
+funcException = false;
+if moxunit_util_platform_is_octave()
+    try
+        func();
+    catch
+        funcException = true;
+        [~,foundID] = lasterr();
+    end
+else
+    try
+        func();
+    catch mexception
+        funcException=true;
+        foundID=mexception.identifier;
+    end
+end
+
+% Check for that exception meeting an id requirement
+if ~funcException
+    error_id = 'moxunit:exceptionNotRaised';
+    if isempty(expectedID)
+        whatswrong = 'exception was not raised';
+    else
+        whatswrong = sprintf('expected exception ''%s'' was not raised',...
+            expectedID);
+    end
     
-    elseif expected_id_passed && ~isequal(expected_id, found_id)
-        whatswrong = sprintf(...
-            'exception ''%s'' was raised, expected ''%s''',...
-            found_id, expected_id);
-        error_id = 'moxunit:wrongExceptionRaised';
-    else
-        return;
-    end
+elseif ~isempty(expectedID) && ~isequal(expectedID, foundID)
+    whatswrong = sprintf(...
+        'exception ''%s'' was raised, expected ''%s''',...
+        foundID, expectedID);
+    error_id = 'moxunit:wrongExceptionRaised';
+else
+    return;
+end
 
-    if nargin<3
-        message='';
-    end
+if nargin<3
+    message='';
+end
 
-    full_message=moxunit_util_input2str(message,whatswrong);
+full_message=moxunit_util_input2str(message,whatswrong);
 
-    if moxunit_util_platform_is_octave()
-        error(error_id,full_message);
-    else
-        throwAsCaller(MException(error_id, full_message));
-    end
+if moxunit_util_platform_is_octave()
+    error(error_id,full_message);
+else
+    throwAsCaller(MException(error_id, full_message));
+end
+
+end
 
