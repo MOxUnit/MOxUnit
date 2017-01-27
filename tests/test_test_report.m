203e55155db8c267fc3c18dc401473769166c62b
Nikolaas N. Oosterhof <nikolaas.oosterhof@unitn.it>
2017-01-27 14:40:34 +0100

    ENH: put space before second character when showing duration

--- tests/test_test_report.m
+++ tests/test_test_report.m
@@ -72,7 +72,7 @@ function test_test_report_output
                 if report_test
                     outcome_str=getOutcomeStr(test_outcome,verbosity);
                     if verbosity==2
-                        outcome_str=sprintf(' %s in %4ds: %s:  %s ',...
+                        outcome_str=sprintf(' %s in %4d s: %s:  %s ',...
                                     outcome_str,ceil(duration),...
                                     name,location);
                     end
@@ -188,7 +188,7 @@ function helper_test_get_statistics_str(format)
                 case 'xml'
                     assertEqual('',s);
 
-                case 'text';
+                case 'text'
                     has_ok=all(counts(failure_pos:n_keys)==0);
 
                     assertEqual(has_ok,contains(s,'OK'));
